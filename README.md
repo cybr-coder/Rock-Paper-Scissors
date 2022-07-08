2e09105152c84a49ae916e44296dbcc600ac26df
cybr-coder <codercybr@gmail.com>
2022-07-08 10:46:01 -0400

    Refactored javascript

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Rock-Paper-Scissors
 
-This is a Rock Paper Scissors game I'm currently working on. This is my First experience with javascript and every time I learn a new topic, I'll be updating the game if it suits.
+This is a Rock Paper Scissors game I'm currently working on. This is my First experience with javascript and every time I learn a new topic, 
+
+Note!!! THIS GAME IS STRICTLY PLAYED IN THE CONSOLE
--- index.js
+++ index.js
@@ -1,13 +1,21 @@
+const playerSelection = playerPlay(window.prompt('Enter your selection'));
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection))
+
 function computerPlay() {
     const random = Math.floor(Math.random() * 3);
-    if (random === 0){
-        return 'rock';
-    } else if (random === 1) {
-        return 'paper';
-    } else {
-        return 'scissors';
-    };
-};
+    switch (random) { 
+        case 0:
+            return 'rock';
+            break;
+        case 1:
+            return 'paper';
+            break;
+        case 2:
+            return 'scissors';
+            break;
+    }
+}
 
 function playerPlay(userInput) {
     userInput = userInput.toLowerCase();
@@ -15,8 +23,8 @@ function playerPlay(userInput) {
         return userInput;
     } else {
         console.log('Error')
-    };
-};
+    }
+}
 
 function playRound(playerSelection, computerSelection) {
    if (playerSelection === computerSelection) {
@@ -27,25 +35,23 @@ function playRound(playerSelection, computerSelection) {
            return 'You Lose!, Paper covers Rock';
        }else {
            return 'You win';
-       };
-   };
+       }
+   }
    if (playerSelection === 'paper') {
        if (computerSelection === 'scissors') {
            return 'You Lose!, Scissors cuts Paper';
        } else {
            return 'You win';
-       };
-   };
+       }
+   }
    if (playerSelection === 'scissors') {
        if (computerSelection === 'rock') {
            return 'You Lose!, Rock crashes Scissors';
        } else{
            return 'You Win';
-       };
-   };
-};
-const playerSelection = playerPlay(window.prompt('Enter your selection'));
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection))
+       }
+   }
+}
+
 
 
